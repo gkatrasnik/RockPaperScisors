8d2c85a2c9216ff209406f056e3422b2646d1a0c
Gasper Katrasnik <gkatrasnik@gmail.com>
2020-11-12 16:20:15 +0100

    basicly finished .js

--- script.js
+++ script.js
@@ -1,90 +1,138 @@
 
 
-    //global variables 
-    var playerPick = "";
-    var finalCompPick = "";
+  //global variables 
+    var userScore =0;
+    var compScore = 0;
 
-    function computerPlay() {
-     // random computer pick
-      const compPick = ["rock", "paper", "scisors"];
+  //button functions
+  document.getElementById("rock").onclick = setPlayerPick;
+  document.getElementById("paper").onclick = setPlayerPick; 
+  document.getElementById("scisors").onclick = setPlayerPick;
 
-      const random = Math.floor(Math.random() * compPick.length);
-      finalCompPick = (compPick[random]);
-      return finalCompPick;
+  function setPlayerPick() {
+    let playerPick = this.id;
+    console.log("Playerpick:",playerPick);
+
+    // ko ti klikneš, se mora zagnat tud computerPlay funkcija, da računalnik zbere... 
+    let computerPick = computerPlay()
+
+    // ko imaš obe spremenljivki (računalnikova izbira + tvoja, rabiš da se zažene primerjava)
+    let whoWon = playRound(playerPick, computerPick)
+    document.getElementById("winner").textContent = whoWon;
+
+    if (whoWon === "Player") {
+      userScore += 1;
+    } else if (whoWon === "Computer") {
+      compScore += 1;
     }
 
-    // player inputs pick, returns lowercase player pick
-    function playerSelection() {      
+    console.log(userScore, compScore);
 
-      while (true) {        
-        playerPick = prompt("Chose your weapon: Rock, Paper or Scisors");
-        playerPick = playerPick.toLowerCase();
-        if (playerPick ==="rock" || playerPick ==="paper" || playerPick ==="scisors") {
-          break;
-        }
-      }
-      return(playerPick);
+    updateScore();
+
+    if (userScore <= 5 || compScore >= 5) {
+      gameEnd();
+    };
+
+  }    
+
+  function updateScore() {
+    document.getElementById("userScore").textContent = userScore;
+    document.getElementById("compScore").textContent = compScore;
+  }
+
+  function gameEnd() {
+    if (userScore>compScore){
+      alert("Player Won " + userScore + " : " + compScore);
+    } else if (userScore<compScore){      
+      alert("Computer Won " + userScore + " : " + compScore);
     }
+    userScore =0;
+    compScore = 0;
+    updateScore();
+  }
 
-    // Plays a round - compares computerPlay vs playerSelection. returns winner. 
-    function playRound() {
-      ps = playerSelection();
-      cp = computerPlay();       
 
-      if (ps=="rock" && cp=="scisors") {
+  function computerPlay() {
+   // random computer pick
+    const compPick = ["rock", "paper", "scisors"];
+
+    const random = Math.floor(Math.random() * compPick.length);
+    finalCompPick = (compPick[random]);
+    console.log("Computerpick:", finalCompPick)
+    return finalCompPick;
+  }
+
+  
+  // Plays a round - compares computerPlay vs playerSelection. returns winner. 
+
+  //  to bi js mrbet tud dav nekak gor, k klikneš, da zbere tvoje, pol komputer zbere, in oba rezultata da v tole funckcijo... in pol jih primerja.
+  
+  // da daš tuki notr playerPick, in computerPick) 
+  function playRound(playerPick, computerPick) {
+    const ps = playerPick; // in po gre v to variablo
+    const cp = computerPick;   // in v to...
+    let winner = "";
+
+    // tuki primerja
+    if (ps=="rock" && cp=="scisors") {
+      winner ="Player";
+      
+    } else if (ps=="paper" && cp=="scisors") {
+        winner ="Computer";
+        
+    } else if (ps=="paper" && cp=="rock") {
         winner ="Player";
         
-      } else if (ps=="paper" && cp=="scisors") {
-          winner ="Computer";
-          
-      } else if (ps=="paper" && cp=="rock") {
-          winner ="Player";
-          
-      } else if (ps=="scisors" && cp=="rock") {
-          winner ="Computer";
-          
-      } else if (ps=="rock" && cp=="paper") {
-          winner ="Computer";
-          
-      } else if (ps=="scisors" && cp=="paper") {
-          winner ="Player";
-          
-      } else {
-          winner ="Nobody"
-      }
-      return winner;
-      
-    }  
+    } else if (ps=="scisors" && cp=="rock") {
+        winner ="Computer";
+        
+    } else if (ps=="rock" && cp=="paper") {
+        winner ="Computer";
+        
+    } else if (ps=="scisors" && cp=="paper") {
+        winner ="Player";
+        
+    } else {
+        winner ="Nobody"
+    }
+    
+    return winner;
+    
+  }
+  
+  
+  
 /*
-    function game() {
+  function game() {
 
-      let playerScore = 0;
-      let computerScore = 0;
-     
-      for (gameCount =0; gameCount<5; gameCount++) {
-        playRound();
-        if (winner==="Player") {
-          playerScore++;
-        } else if (winner==="Computer") {
-          computerScore++;
-        } 
-        console.log(playerPick + " : " + finalCompPick + " | "+ winner + " wins this round");
-        console.log("Player " + playerScore + " : " + computerScore + " Computer");
-      }
+    let playerScore = 0;
+    let computerScore = 0;
+   
+    for (gameCount =0; gameCount<5; gameCount++) {
+      playRound();
+      if (winner==="Player") {
+        playerScore++;
+      } else if (winner==="Computer") {
+        computerScore++;
+      } 
+      console.log(playerPick + " : " + finalCompPick + " | "+ winner + " wins this round");
+      console.log("Player " + playerScore + " : " + computerScore + " Computer");
+    }
 
-    
-      if (playerScore>computerScore) {
-        return("Player wins " + playerScore + " : " + computerScore);
-      } else if (playerScore<computerScore) {
-        return("Computer wins " + playerScore + " : " + computerScore);
-      } else {
-        return("It's a tie! " + playerScore + " : " + computerScore);
-      }
+  
+    if (playerScore>computerScore) {
+      return("Player wins " + playerScore + " : " + computerScore);
+    } else if (playerScore<computerScore) {
+      return("Computer wins " + playerScore + " : " + computerScore);
+    } else {
+      return("It's a tie! " + playerScore + " : " + computerScore);
+    }
 
-      // reset scores, useful when playing in console.
-      playerScore = 0;
-      computerScore = 0;
-    
+    // reset scores, useful when playing in console.
+    playerScore = 0;
+    computerScore = 0;
+  
 
-    } 
- */   
+  } 
+*/   
